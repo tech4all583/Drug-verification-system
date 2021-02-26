71ba462de7e085e358c29f3290344bdb90729d8c
Tech4All <tech4all583@gmail.com>
2021-02-26 08:31:37 +0100

    initail

--- register.php
+++ register.php
@@ -11,4 +11,25 @@ require_once 'config/core.php';
 require_once 'libs/head.php';
 ?>
 
+<section class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <ul class="breadcrumb">
+                    <li><a href="<?= base_url() ?>">Home</a></li>
+                    <li class="active"><span id="lblPageTitle">Company Profile</span>
+                        |<span id="lblPageDescription">Create Profile</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h1>Register as a Client</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <?php require_once 'libs/foot.php';?>
